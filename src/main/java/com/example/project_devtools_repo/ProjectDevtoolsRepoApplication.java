127697585e299d387769478a15f6d1b53e75499f
verabradu <verabradu92@gmail.com>
2025-05-19 09:00:08 -0500

    Initial commit

---
+++ src/main/java/com/example/project_devtools_repo/ProjectDevtoolsRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_devtools_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDevtoolsRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDevtoolsRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/project_devtools_repo/ProjectDevtoolsRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_devtools_repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDevtoolsRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
